dc0a6e7981f010100913d5d8d785613a89e01433
Joseph Archer <viewtiful.kja@gmail.com>
2022-11-28 08:48:33 -0500

    Added the post and login route functionality

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const app = express();
 
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+app.use(express.json())
 
-app.get('/posts', (req, res) => {
+// Test Login Data
+const posts = [
+    {
+        username: 'Joseph',
+        title: 'Test1'
+    },
+    {
+        username: 'Archer',
+        title: 'Test2'
+    }
+]
 
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const user = { name: username }
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
+})
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token === null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+
+}
 
 app.listen(3000)
